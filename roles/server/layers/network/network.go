9600751e2e251111d489b7642710b64e9772dbc0
aoikida <aoikida@sfc.keio.ac.jp>
2024-08-09 08:44:51 +0900

    Add licence

--- roles/server/layers/network/network.go
+++ roles/server/layers/network/network.go
@@ -1,4 +1,20 @@
 /*
+   Copyright 2024 Aoi Kida
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
    Copyright 2021 Rabia Research Team and Developers
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -114,9 +130,12 @@ MainLoop:
 	for {
 		select {
 		case <-n.Done:
+			fmt.Printf("network = %d received done signal\n", n.SvrId)
 			break MainLoop
 
 		case msg := <-n.ProxyIn: // ClientRequest msg
+			//msgのコマンドを出力する
+			//fmt.Printf("msg: %v\n", msg.Obj.Commands)
 			n.ToSerializer <- msg
 
 		case msg := <-n.MsgHandlerIn: // ProposalReply msg
